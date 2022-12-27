782009f29d5af5b51eb1f443e105a5a49d6e0fb9
tiago479 <76876635+tiago479@users.noreply.github.com>
2022-12-27 14:38:46 +0000

    added table module and extras module

---
+++ projectIS/projectIS/projectIS/Model/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projectIS.Model
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Created_at { get; set; }
+        public int Application_Id { get; set; }
+    }
+}
