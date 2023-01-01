59138ed52b78479a872d601ce7238a20fae15779
tiago479 <76876635+tiago479@users.noreply.github.com>
2023-01-01 19:59:36 +0000

    appA faltam alguns retoques

--- projectIS/projectIS/AppA/Form1.Designer.cs
+++ projectIS/projectIS/AppA/Form1.Designer.cs
@@ -29,29 +29,37 @@
         private void InitializeComponent()
         {
             this.applicationName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.createButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.findById = new System.Windows.Forms.TextBox();
+            this.showById = new System.Windows.Forms.TextBox();
+            this.getAllApps = new System.Windows.Forms.Button();
+            this.getAllAppsBox = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.getAllModules = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.moduleByIdGet = new System.Windows.Forms.TextBox();
+            this.getAllMods = new System.Windows.Forms.TextBox();
+            this.moduleId = new System.Windows.Forms.TextBox();
+            this.moduleName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ModuleNames = new System.Windows.Forms.Label();
+            this.resType = new System.Windows.Forms.TextBox();
+            this.applicationModule = new System.Windows.Forms.Label();
+            this.textBoxApplicationModule = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // applicationName
             // 
-            this.applicationName.Location = new System.Drawing.Point(12, 50);
+            this.applicationName.Location = new System.Drawing.Point(12, 31);
             this.applicationName.Name = "applicationName";
             this.applicationName.Size = new System.Drawing.Size(100, 22);
             this.applicationName.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Name:";
-            // 
             // createButton
             // 
-            this.createButton.Location = new System.Drawing.Point(15, 79);
+            this.createButton.Location = new System.Drawing.Point(12, 59);
             this.createButton.Name = "createButton";
             this.createButton.Size = new System.Drawing.Size(75, 23);
             this.createButton.TabIndex = 2;
@@ -59,13 +67,180 @@
             this.createButton.UseVisualStyleBackColor = true;
             this.createButton.Click += new System.EventHandler(this.createButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "btnFindById";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // findById
+            // 
+            this.findById.Location = new System.Drawing.Point(205, 3);
+            this.findById.Name = "findById";
+            this.findById.Size = new System.Drawing.Size(29, 22);
+            this.findById.TabIndex = 4;
+            // 
+            // showById
+            // 
+            this.showById.Location = new System.Drawing.Point(131, 59);
+            this.showById.Multiline = true;
+            this.showById.Name = "showById";
+            this.showById.Size = new System.Drawing.Size(196, 88);
+            this.showById.TabIndex = 5;
+            // 
+            // getAllApps
+            // 
+            this.getAllApps.Location = new System.Drawing.Point(538, 12);
+            this.getAllApps.Name = "getAllApps";
+            this.getAllApps.Size = new System.Drawing.Size(75, 23);
+            this.getAllApps.TabIndex = 6;
+            this.getAllApps.Text = "getAllApps";
+            this.getAllApps.UseVisualStyleBackColor = true;
+            this.getAllApps.Click += new System.EventHandler(this.getAllApps_Click);
+            // 
+            // getAllAppsBox
+            // 
+            this.getAllAppsBox.Location = new System.Drawing.Point(538, 41);
+            this.getAllAppsBox.Name = "getAllAppsBox";
+            this.getAllAppsBox.Size = new System.Drawing.Size(250, 254);
+            this.getAllAppsBox.TabIndex = 7;
+            this.getAllAppsBox.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 51);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "create Module";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(159, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "btnFindById";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // getAllModules
+            // 
+            this.getAllModules.Location = new System.Drawing.Point(334, 421);
+            this.getAllModules.Name = "getAllModules";
+            this.getAllModules.Size = new System.Drawing.Size(99, 33);
+            this.getAllModules.TabIndex = 10;
+            this.getAllModules.Text = "getAllModes";
+            this.getAllModules.UseVisualStyleBackColor = true;
+            this.getAllModules.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 273);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // moduleByIdGet
+            // 
+            this.moduleByIdGet.Location = new System.Drawing.Point(151, 273);
+            this.moduleByIdGet.Name = "moduleByIdGet";
+            this.moduleByIdGet.Size = new System.Drawing.Size(83, 22);
+            this.moduleByIdGet.TabIndex = 12;
+            // 
+            // getAllMods
+            // 
+            this.getAllMods.Location = new System.Drawing.Point(300, 261);
+            this.getAllMods.Multiline = true;
+            this.getAllMods.Name = "getAllMods";
+            this.getAllMods.Size = new System.Drawing.Size(173, 154);
+            this.getAllMods.TabIndex = 13;
+            // 
+            // moduleId
+            // 
+            this.moduleId.Location = new System.Drawing.Point(184, 216);
+            this.moduleId.Name = "moduleId";
+            this.moduleId.Size = new System.Drawing.Size(28, 22);
+            this.moduleId.TabIndex = 14;
+            // 
+            // moduleName
+            // 
+            this.moduleName.Location = new System.Drawing.Point(373, 205);
+            this.moduleName.Name = "moduleName";
+            this.moduleName.Size = new System.Drawing.Size(100, 22);
+            this.moduleName.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "resType";
+            // 
+            // ModuleNames
+            // 
+            this.ModuleNames.AutoSize = true;
+            this.ModuleNames.Location = new System.Drawing.Point(271, 208);
+            this.ModuleNames.Name = "ModuleNames";
+            this.ModuleNames.Size = new System.Drawing.Size(96, 16);
+            this.ModuleNames.TabIndex = 17;
+            this.ModuleNames.Text = "ModuleNames";
+            // 
+            // resType
+            // 
+            this.resType.Location = new System.Drawing.Point(373, 233);
+            this.resType.Name = "resType";
+            this.resType.Size = new System.Drawing.Size(101, 22);
+            this.resType.TabIndex = 18;
+            // 
+            // applicationModule
+            // 
+            this.applicationModule.AutoSize = true;
+            this.applicationModule.Location = new System.Drawing.Point(60, 179);
+            this.applicationModule.Name = "applicationModule";
+            this.applicationModule.Size = new System.Drawing.Size(161, 16);
+            this.applicationModule.TabIndex = 19;
+            this.applicationModule.Text = "application Module Name";
+            // 
+            // textBoxApplicationModule
+            // 
+            this.textBoxApplicationModule.Location = new System.Drawing.Point(227, 179);
+            this.textBoxApplicationModule.Name = "textBoxApplicationModule";
+            this.textBoxApplicationModule.Size = new System.Drawing.Size(112, 22);
+            this.textBoxApplicationModule.TabIndex = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.textBoxApplicationModule);
+            this.Controls.Add(this.applicationModule);
+            this.Controls.Add(this.resType);
+            this.Controls.Add(this.ModuleNames);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.moduleName);
+            this.Controls.Add(this.moduleId);
+            this.Controls.Add(this.getAllMods);
+            this.Controls.Add(this.moduleByIdGet);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.getAllModules);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.getAllAppsBox);
+            this.Controls.Add(this.getAllApps);
+            this.Controls.Add(this.showById);
+            this.Controls.Add(this.findById);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.createButton);
             this.Controls.Add(this.applicationName);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -77,8 +252,25 @@
         #endregion
 
         private System.Windows.Forms.TextBox applicationName;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox findById;
+        private System.Windows.Forms.TextBox showById;
+        private System.Windows.Forms.Button getAllApps;
+        private System.Windows.Forms.RichTextBox getAllAppsBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button getAllModules;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox moduleByIdGet;
+        private System.Windows.Forms.TextBox getAllMods;
+        private System.Windows.Forms.TextBox moduleId;
+        private System.Windows.Forms.TextBox moduleName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ModuleNames;
+        private System.Windows.Forms.TextBox resType;
+        private System.Windows.Forms.Label applicationModule;
+        private System.Windows.Forms.TextBox textBoxApplicationModule;
     }
 }
 
--- projectIS/projectIS/AppA/Form1.cs
+++ projectIS/projectIS/AppA/Form1.cs
@@ -14,18 +14,58 @@ using System.Xml.Linq;
 using System.Xml;
 using RestSharp;
 using System.Security.AccessControl;
-using System.Text.Json.Serialization;
+using projectIS.Model;
+using Application = projectIS.Model.Application;
+using Module = projectIS.Model.Module;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+using RestSharp.Serializers.Xml;
+using RestSharp.Serializers;
+using System.Data.SqlTypes;
 
 namespace AppA
 {
     public partial class Form1 : Form
     {
-        string url = @"http://localhost:54833/api/somiod";
+        string url = "https://localhost:44356/api/somiod/";
         public Form1()
         {
             InitializeComponent();
         }
 
+        #region Aux Functions
+
+        //inicio metodo para tirar "lixo" do xml
+        public static string RemoveAllNamespaces(string xmlDocument) //colocar no tipo para puder remover namespaces
+        {
+            XElement xmlDocumentWithoutNs = RemoveAllNamespaces(XElement.Parse(xmlDocument)); //esta no tipo que precisamos ja podemos fazer alteracao de xml vamos para metodo de baixo (core function recursio)
+
+            return xmlDocumentWithoutNs.ToString();
+        }
+
+        //metodo para tirar "lixo" do xml
+        private static XElement RemoveAllNamespaces(XElement xmlDocument) //remover namespaces
+        {
+            if (!xmlDocument.HasElements) //se nao tiver elementos filho entra
+            {
+                XElement xElement = new XElement(xmlDocument.Name.LocalName);
+                xElement.Value = xmlDocument.Value;
+
+                foreach (XAttribute attribute in xmlDocument.Attributes())
+                    xElement.Add(attribute);
+
+                return xElement;
+            }
+            return new XElement(xmlDocument.Name.LocalName, xmlDocument.Elements().Select(el => RemoveAllNamespaces(el))); //recursivo .... 0> (lambda expression(=>) method call para o parametro el ...)
+        }
+
+        #endregion
+
+        #region APPLICATION
+
+        //CREATE APPLICATION
         private void createButton_Click(object sender, EventArgs e)
         {
             XmlDocument applicationXml = new XmlDocument();
@@ -41,5 +81,184 @@ namespace AppA
 
             MessageBox.Show(response.ResponseStatus.ToString());
         }
+
+        //GET MOD BY ID
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var id = findById.Text;
+
+            if (String.IsNullOrEmpty(id) || Int32.Parse(id) < 1)
+                return;
+
+            var client = new RestClient(url);
+            
+            var request = new RestRequest($"{id}", RestSharp.Method.Get);
+            request.AddHeader("Accept", "application/xml");
+
+            var response = client.Execute(request);
+
+
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                
+                var result = XElement.Parse(response.Content).Value; //se nao fizer isto aparece o microsoft shit
+
+                string xmlWellFormated = RemoveAllNamespaces(result);
+
+                // Create an instance of the XmlSerializer class
+                XmlSerializer serializer = new XmlSerializer(typeof(Application));
+
+                using (StringReader reader = new StringReader(xmlWellFormated))
+                {
+                    Application app = (Application)serializer.Deserialize(reader);
+
+                    if (app.Id == 0)
+                    {
+                        showById.AppendText($"Application not found {Environment.NewLine}");
+                    }
+                    else
+                    {
+                        showById.AppendText($"created at: {app.Id} {Environment.NewLine} Application Name: {app.Name} {Environment.NewLine} created at: {app.Creation_dt}");
+                    }
+                }
+            }
+
+        }
+
+        //GET ALL APPS
+        private void getAllApps_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient(url);
+
+            var request = new RestRequest($"", RestSharp.Method.Get);
+            request.AddHeader("Accept", "application/xml");
+
+            var response = client.Execute(request);
+
+            var result = XElement.Parse(response.Content).Value; //se nao fizer isto aparece o microsoft shit
+
+            string xmlWellFormated = RemoveAllNamespaces(result);
+
+            // Create an instance of the XmlSerializer class
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Application>));
+
+            using (StringReader reader = new StringReader(xmlWellFormated))
+            {
+                List<Application> Applications = (List<Application>)serializer.Deserialize(reader);
+
+                foreach (Application app in Applications)
+                if (app.Id == 0)
+                {
+                    getAllAppsBox.AppendText($"Application not found {Environment.NewLine}");
+                }
+                else
+                {
+                    getAllAppsBox.AppendText($"created at: {app.Id} {Environment.NewLine} Application Name: {app.Name} {Environment.NewLine} created at: {app.Creation_dt}");
+                }
+            }
+        }
+        #endregion
+
+        #region MODULE
+
+        //CREATE MODULE
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string appName = textBoxApplicationModule.Text;
+
+            XmlDocument moduleXml = new XmlDocument();
+            XmlElement moduleElement = (XmlElement)moduleXml.AppendChild(moduleXml.CreateElement("Module"));
+            moduleElement.AppendChild(moduleXml.CreateElement("Name")).InnerText = textBox1.Text;
+            moduleElement.AppendChild(moduleXml.CreateElement("Res_type")).InnerText = "module";
+            Console.WriteLine(moduleXml.OuterXml);
+
+            var client = new RestSharp.RestClient(url);
+            var request = new RestSharp.RestRequest($"{appName}", RestSharp.Method.Post);
+            request.RequestFormat = RestSharp.DataFormat.Xml;
+            request.AddParameter("module/xml", moduleXml, ParameterType.RequestBody);
+            RestSharp.RestResponse response = client.Execute(request);
+
+            MessageBox.Show(response.ResponseStatus.ToString());
+        }
+
+        //GET MODULE BY ID
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var id = moduleId.Text;
+            string appName = textBoxApplicationModule.Text;
+
+            if (String.IsNullOrEmpty(id) || Int32.Parse(id) < 1)
+                return;
+
+            var client = new RestClient(url);
+
+            var request = new RestRequest($"{appName}/{id}", RestSharp.Method.Get);
+            request.AddHeader("Accept", "module/xml");
+
+            var response = client.Execute(request);
+
+
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = XElement.Parse(response.Content).Value; //se nao fizer isto aparece o microsoft shit
+
+                string xmlWellFormated = RemoveAllNamespaces(result);
+
+                // Create an instance of the XmlSerializer class
+                XmlSerializer serializer = new XmlSerializer(typeof(Module));
+
+                using (StringReader reader = new StringReader(xmlWellFormated))
+                {
+                    Module mod = (Module)serializer.Deserialize(reader);
+
+                    if (mod.Id == 0)
+                    {
+                        moduleByIdGet.AppendText($"Module not found {Environment.NewLine}");
+                    }
+                    else
+                    {
+                        moduleByIdGet.AppendText($"created at: {mod.Id} {Environment.NewLine} Module Name: {mod.Name} {Environment.NewLine} created at: {mod.Creation_dt} {Environment.NewLine} module parent: {mod.Parent}");
+                    }
+                }
+            }
+        }
+
+        //GET ALL MODULES
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient(url); 
+            string appName = textBoxApplicationModule.Text;
+
+            var request = new RestRequest($"{appName}", RestSharp.Method.Get);
+            request.AddHeader("Accept", "module/xml");
+
+            var response = client.Execute(request);
+
+            var result = XElement.Parse(response.Content).Value; //se nao fizer isto aparece o microsoft shit
+
+            string xmlWellFormated = RemoveAllNamespaces(result);
+
+            // Create an instance of the XmlSerializer class
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Module>));
+
+            using (StringReader reader = new StringReader(xmlWellFormated))
+            {
+                List<Module> modules = (List<Module>)serializer.Deserialize(reader);
+
+                foreach (Module mod in modules)
+                    if (mod.Id == 0)
+                    {
+                        getAllMods.AppendText($"Module not found {Environment.NewLine}");
+                    }
+                    else
+                    {
+                        getAllMods.AppendText($"created at: {mod.Id} {Environment.NewLine} Module Name: {mod.Name} {Environment.NewLine} created at: {mod.Creation_dt} {Environment.NewLine} module parent: {mod.Parent}");
+                    }
+            }
+        }
+
+        #endregion
     }
 }
